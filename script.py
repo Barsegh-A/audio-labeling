29db232dbd502a0c3d43673fa0ddfb058bb6ba21
barseghatanyan <barsegh.atanyan@tumo.org>
2021-10-20 09:58:02 +0400

    cleanup

--- script.py
+++ script.py
@@ -5,14 +5,14 @@ import pandas as pd
 path = "audio/" # path to the dataset
 files = os.listdir(path)
 
-df = pd.DataFrame([], columns = ['file_name', 'label'])
+df = pd.DataFrame([], columns = ["file_name", "label"])
 
 for file, i in zip(files, range(len(files))):
 	print("Currently playing " + file)
 	playsound(path + file)
 	label = input("Please, provide the label(n for noisy and c for clean audio files): ")
-	while(label != 'c' and label != "n"):
+	while(label != "c" and label != "n"):
 		label = input("Provided label is neither n nor c. Try again... ")
 	df.loc[i] = [file, label]
 
-df.to_json("data.json", orient = 'records')
+df.to_json("data.json", orient = "records")
